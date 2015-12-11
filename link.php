26241e0e434ec5646fd48e53456cb97c6a4ca1c1
azik <azer.mehdiyev28@gmail.com>
2015-12-11 12:42:55 +0400

    style.css failin adi deyishi styles.css-e. Ashagidaki barlar elave olundu

--- link.php
+++ link.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="css/tag_style.css">
+
