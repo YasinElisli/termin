5f2d6461b0148151a703f120ff034dcda576b51b
azik <azer.mehdiyev28@gmail.com>
2015-12-12 02:25:31 +0400

    Conflict hell olundu. Yeniden popu kodlari elave olundu. User inputlari deyishildi amma bazaya goshulmayib

--- link.php
+++ link.php
@@ -1,12 +1,11 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<<<<<<< HEAD
-<!-- <link rel="stylesheet" type="text/css" href="css/styles.css">
-<link rel="stylesheet" type="text/css" href="css/tag_style.css"> -->
+
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="css/tag_style.css">
 <link rel="stylesheet" type="text/css" href="css/popup.css">
 
-=======
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
 <!-- <link rel="stylesheet" type="text/css" href="css/tag_style.css"> -->
->>>>>>> 19e06ad8a1530ef2cb5a3dd9ba53290250c3c571
+
