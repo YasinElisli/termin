a866670c77cc24bc1501541ad9630caad86ff52e
Yasin Elisli <e.yasin@code.edu.az>
2015-12-16 19:21:51 +0400

    butun sehifelere ob_start() elave oldu

--- profile/profile.php
+++ profile/profile.php
@@ -1,3 +1,4 @@
+<?php ob_start();?>
 <?php
     include 'core.php';
     // include 'user.php';
--- termin.php
+++ termin.php
@@ -1,3 +1,4 @@
+<?php ob_start();?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -24,7 +25,7 @@
     <a href="#"><i class="fa fa-star  fa-lg"></i></a>
     <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
             <a href="#"><i class="fa fa-facebook-official fa-lg"></i></a>
-            <a href="#"><i class="fa fa-google-plus fa-lg"></i></a>       
+            <a href="#"><i class="fa fa-google-plus fa-lg"></i></a>
         <a href="#"><i class="fa fa-share-alt fa-lg"></i></a> </div>
         <div class="demo"><?php echo $termin ?></div>
         <div class="izah"><?php echo $termin_desc ?></div>
@@ -38,7 +39,7 @@
 
       </div>
 
-        </div> 
+        </div>
       </div>
         </div>
           <div class="row pull-right">
@@ -54,7 +55,7 @@
 
 </div>
 
-  
+
 
 
 
