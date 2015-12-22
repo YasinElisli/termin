52f4b27743877f81e7e9e43547d24076533128ae
Yasin Elisli <e.yasin@code.edu.az>
2015-12-22 12:55:31 +0400

    validation.php-e  signin() elave olundu

--- core.php
+++ core.php
@@ -257,6 +257,60 @@ function tags(){
       }
 
 		}
+    function signin($user_name,$user_password) {
 
+  		include 'db.php';
+  		echo "username: ".$user_name."<br>";
+  		echo "password: ".$user_password."<br>";
+  		$sql = "SELECT * FROM user WHERE username='$user_name'";
+  		$query = mysqli_query($db_connection, $sql);
+
+  		$numrow = mysqli_num_rows($query);
+
+  		if ($numrow === 1) {
+
+  			while ($row = mysqli_fetch_assoc($query)) {
+  				$dbusername = $row['username'];
+  				$dbpassword = $row['password'];
+  				$dbregdate = $row['reg_date'];
+  				$dbuserID = $row['id'];
+  				$dbFirst = $row['firstname'];
+  				$dbLast = $row['lastname'];
+  				$dbMail = $row['email'];
+  				$dbBirth = $row['birthdate'];
+  				$dbGender = $row['gender'];
+  			}
+
+  			echo "username ".$user_name." dbusername ". $dbusername."<br>";
+  			echo "password ".$user_password." dbpassword ". $dbpassword."<br>";
+  			//check with the given data
+
+  			if ($dbusername==$user_name && $dbpassword==$user_password) {
+  				echo "You are now logged in<br>";
+  				/*session-a yuklenen butun datalar profile sehifesinde
+  				 * lazim olacag ki muvafig yerlerde gosterilsin
+  				 */
+  				if (!isset($_SESSION)) {
+          			session_start();
+     				}
+
+  				$_SESSION['username'] = $user_name;
+  				$_SESSION['user_id'] = $dbuserID;
+  				$_SESSION['user_reg_date'] = $dbregdate;
+  				$_SESSION['firstname'] = $dbFirst;
+  				$_SESSION['lastname'] = $dbLast;
+  				$_SESSION['email'] = $dbMail;
+  				$_SESSION['pass'] = $dbpassword;
+  				$_SESSION['birthdate'] = $dbBirth;
+  				$_SESSION['gender'] = $dbGender;
+  				//redirect user to new Welcome user page
+  				header('Location: profile.php?id=1');
+  			} else {
+  				echo "incorrect password";
+  			}
+   		} else {
+  			die("That user does not exist");
+  		}
+  	}
 
  ?>
--- profile/profileedit.php
+++ profile/profileedit.php
@@ -1,28 +1,44 @@
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 	<div class="prf-edt">
 				<div>
-					<label><p class="prf-edt-paragr">Username</p><input value="<?php echo $_SESSION['username']; ?>" class="form-control" type="text" name""></label>
+					<label><p class="prf-edt-paragr">Username</p>
+						<input value="<?php echo $_SESSION['username']; ?>" class="form-control" type="text" name"">
+					</label>
 				</div>
 				<div>
-					<label><p class="prf-edt-paragr">Adınız</p><input value="<?php echo $_SESSION['firstname']; ?>" class="form-control" type="text" name""></label>
+					<label><p class="prf-edt-paragr">Adınız</p>
+						<input value="<?php echo $_SESSION['firstname']; ?>" class="form-control" type="text" name"">
+					</label>
 				</div>
 				<div>
-					<label><p class="prf-edt-paragr">Soyadınız</p><input value="<?php echo $_SESSION['lastname']; ?>" class="form-control" type="text" name""></label>
+					<label><p class="prf-edt-paragr">Soyadınız</p>
+						<input value="<?php echo $_SESSION['lastname']; ?>" class="form-control" type="text" name"">
+					</label>
 				</div>
 				<div>
-					<label><p class="prf-edt-paragr">E-mail</p><input value="<?php echo " ".$_SESSION['email']; ?>" class="form-control" style="padding:0px;" type="mail" name""></label>
+					<label><p class="prf-edt-paragr">E-mail</p>
+						<input value="<?php echo " ".$_SESSION['email']; ?>" class="form-control" style="padding:0px;" type="mail" name"">
+					</label>
 				</div>
 				<div>
-					<label><p class="prf-edt-paragr">Köhnə parol</p><input value="<?php echo $_SESSION['pass']; ?>" class="form-control" type="password" name""></label>
+					<label><p class="prf-edt-paragr">Köhnə parol</p>
+						<input value="<?php echo $_SESSION['pass']; ?>" class="form-control" type="password" name"">
+					</label>
 				</div>
 				<div>
-					<label><p class="prf-edt-paragr">Yeni Parol</p><input class="form-control" type="password" name""></label>
+					<label><p class="prf-edt-paragr">Yeni Parol</p>
+						<input class="form-control" type="password" name"">
+					</label>
 				</div>
 				<div>
-					<label><p class="prf-edt-paragr">Yeni Parol Təkrarı</p><input class="form-control" type="password" name""></label>
+					<label><p class="prf-edt-paragr">Yeni Parol Təkrarı</p>
+						<input class="form-control" type="password" name"">
+					</label>
 				</div>
 				<div>
-					<label><p class="prf-edt-paragr">Birthday</p><input value="<?php echo $_SESSION['birthdate']; ?>" class="form-control dateinput" type="date" name""></label>
+					<label><p class="prf-edt-paragr">Birthday</p>
+						<input value="<?php echo $_SESSION['birthdate']; ?>" class="form-control dateinput" type="date" name"">
+					</label>
 				</div>
 				<div>
 					<Strong><p class="prf-edt-paragr">Gender</p></Strong>
