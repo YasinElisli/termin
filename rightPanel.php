fa39d5080f62a85ab0e2d71f1a04a14bec498ffa
Yasin Elisli <e.yasin@code.edu.az>
2015-12-25 16:06:19 +0400

    termin sehifesi dizayn debug

--- rightPanel.php
+++ rightPanel.php
@@ -1,58 +1,35 @@
 <div id="statistic" class="container-fluid">
-        <div class="panel panel-default most_search_term">
-            <div class="panel-heading">En cox baxilan 5 termin</div>
-            <ul class="list-group">
+        <ul class="list-group most_search_term">
+            <li class="list-group-item">En cox baxilan 5 termin</li>
+
               <?php $queryMostReadRight = mostRead();
               $sira = 0;
               while ($termin2 = mysqli_fetch_assoc($queryMostReadRight)) {
                 $sira++;
-                echo '<li class="list-group-item mostReadItem">'.$sira.'.'.$termin2['termin'].'</p>';
+                echo '<li class="list-group-item mostReadItem"><p>'.$sira.'.'.$termin2['termin'].'</p></li>';
               }; ?>
+          </ul>
 
+          <ul class="list-group most_search_term">
+              <li class="list-group-item">En cox baxilan 5 termin</li>
 
+                <?php $queryMostReadRight = mostRead();
+                $sira = 0;
+                while ($termin2 = mysqli_fetch_assoc($queryMostReadRight)) {
+                  $sira++;
+                  echo '<li class="list-group-item mostReadItem"><p>'.$sira.'.'.$termin2['termin'].'</p></li>';
+                }; ?>
             </ul>
-            </div>
-        </div>
-        <div class="panel panel-default newest_term">
-            <div class="panel-heading">En son terminler</div>
-            <div class="panel-body">
 
-            <div class="disp-block">
-            <span class="a-clas">1</span>
-            <span class="a-clas">2</span>
-            <span class="a-clas">3</span>
-            <span class="a-clas">4</span>
-            <span class="a-clas">5</span>
-            <span class="a-clas">6</span>
-            <span class="a-clas">7</span>
-            <span class="a-clas">8</span>
-            <span class="a-clas">9</span>
-            <span class="a-clas">10</span>
-            </div>
-            <div class="disp-block"><a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                    <a class="a-clas" href="#">Lorem ipsum</a>
-                </div>
+        <ul class="list-group top_rated_wr">
+            <li class="list-group-item">En reytingli 5 yazar</li>
 
-            </div>
-        </div>
-        <div class="panel panel-default top_rated_wr">
-            <div class="panel-heading">En reytingli 5 yazar</div>
-            <ul class="list-group">
               <?php $queryBestWriteRight = bestWriter();
               $sira2 = 0;
               while ($termin3 = mysqli_fetch_assoc($queryBestWriteRight)) {
                 $sira2++;
-                echo '<li class="list-group-item mostReadItem">'.$sira2.'.'.$termin3['username'].'</p>';
+                echo '<li class="list-group-item mostReadItem"><p>'.$sira2.'.'.$termin3['username'].'</p></li>';
               }; ?>
+
             </ul>
-            </div>
         </div>
-    </div>
--- termin.php
+++ termin.php
@@ -11,73 +11,116 @@
     <?php include 'searchWords.php';
           include 'header.php';
           include 'core.php'; ?>
-      <div class="container terminMain">
+      <div class="container-fluid terminMain">
         <div class="row">
           <div class="col-lg-12 padreset">
             <div class="topBanner"><center>1024 X 100 </center></div>
           </div>
         </div>
-        <div class="row pull-left">
-          <div class="col-lg-12 padreset">
-            <div class="container-fluid">
-          <div class="month row">
-            <div class="mm">Daxil edilib: <?php echo $publ_date ?></div>
- <div class="term">Kategoriya: <?php echo $kategoriya ?></div>
-   <ul class="sosial pull-right">
-      <li><a href="#"><i class="fa fa-star  fa-lg"></i></a></li>
-      <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
-      <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
-      <li><a href="#"><i class="fa fa-google-plus fa-lg"></i></a></li>
-      <li><a href="#"><i class="fa fa-share-alt fa-lg"></i></a></li>
-   </ul>
-        <div class="demo"><?php echo "<h3 class=\"terName\">".$termin."</h3>" ?></div>
-        <div class="izah"><?php echo $termin_desc ?></div>
-      <div class="button">
-        <ul class="likeUnlike">
-          <li>
-            <button class="btn btn-success" id="presslike" onclick="userLiked(<?php echo $termin_id ?>,<?php echo $termin_like ?>,<?php echo $like ?>)">
-                <i class="fa fa-thumbs-o-up fa-lg"></i><span id="num_like"><?php echo $termin_like ?></span>
-            </button>
-          </li>
-          <li>
-            <a class="btn btn-danger" href="#" >
-            <i class="fa fa-thumbs-o-down fa-lg"></i> 11</a>
-          </li>
-
-        </ul>
-
-      </div>
-      <div class="yazar" >Əlavə etdi: <?php echo $user_name ?></div>
-      <div class="random"><button type="button" class="btn btn-secondary">Random Termin</button></div>
-
-      </div>
-
+        <div class="row">
+          <div class="col-md-8 terminLeft">
+            <div class="col-md-12 pull-left terminResult ">
+              <ul class="pubCat pull-left">
+                <li class="mm ">Daxil edilib: <?php echo $publ_date ?></li>
+                <li class="term">Kategoriya: <?php echo $kategoriya ?></li>
+              </ul>
+              <ul class="sosial pull-right">
+                <li><a href="#"><i class="fa fa-star  fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-share-alt fa-lg"></i></a></li>
+              </ul>
+              <div class="demo"><?php echo "<h3 class=\"terName\">".$termin."</h3>" ?></div>
+              <div class="izah"><?php echo $termin_desc ?></div>
+              <div class="button">
+              <ul class="likeUnlike">
+                <li>
+                  <button class="btn btn-success" id="presslike" onclick="userLiked(<?php echo $termin_id ?>,<?php echo $termin_like ?>,<?php echo $like ?>)">
+                      <i class="fa fa-thumbs-o-up fa-lg"></i><span id="num_like"><?php echo $termin_like ?></span>
+                  </button>
+                </li>
+                <li>
+                  <a class="btn btn-danger" href="#" >
+                  <i class="fa fa-thumbs-o-down fa-lg"></i> 11</a>
+                </li>
+                <li class="yazar" >Əlavə etdi: <?php echo $user_name ?></li>
+                <li class="menbe"> Mənbə : Yazar</li>
+                <li class="random"><button type="button" class="btn btn-secondary">Random Termin</button></li>
+                </li>
+              </ul>
+              </div>
+            </div>
+            <div class="col-md-12 pull-left sliderTermin"></div>
+          </div>
+          <div class="col-md-4 terminRight pull-right">
+            <div class="col-md-12 ">
+              <?php
+                include "rightPanel.php";
+              ?>
+            </div>
+          </div>
         </div>
       </div>
-        </div>
-          <div class="row pull-right">
-            <div class="col-lg-12 padreset">
-        <?php 
-          // <div id="statistic" class="container-fluid">
-          include "rightPanel.php"; 
-        ?>
-      </div>
-  </div>
+
+
+
+<!-- evvelki kodlar  -->
+<!-- <div class="row pull-left">
+  <div class="col-lg-12 padreset">
+    <div class="container-fluid">
+  <div class="month row">
+    <div class="mm">Daxil edilib: <?php echo $publ_date ?></div>
+<div class="term">Kategoriya: <?php echo $kategoriya ?></div>
+<ul class="sosial pull-right">
+<li><a href="#"><i class="fa fa-star  fa-lg"></i></a></li>
+<li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+<li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+<li><a href="#"><i class="fa fa-google-plus fa-lg"></i></a></li>
+<li><a href="#"><i class="fa fa-share-alt fa-lg"></i></a></li>
+</ul>
+<div class="demo"><?php echo "<h3 class=\"terName\">".$termin."</h3>" ?></div>
+<div class="izah"><?php echo $termin_desc ?></div>
+<div class="button">
+<ul class="likeUnlike">
+  <li>
+    <button class="btn btn-success" id="presslike" onclick="userLiked(<?php echo $termin_id ?>,<?php echo $termin_like ?>,<?php echo $like ?>)">
+        <i class="fa fa-thumbs-o-up fa-lg"></i><span id="num_like"><?php echo $termin_like ?></span>
+    </button>
+  </li>
+  <li>
+    <a class="btn btn-danger" href="#" >
+    <i class="fa fa-thumbs-o-down fa-lg"></i> 11</a>
+  </li>
+
+</ul>
+
+</div>
+<div class="yazar" >Əlavə etdi: <?php echo $user_name ?></div>
+<div class="random"><button type="button" class="btn btn-secondary">Random Termin</button></div>
+
 </div>
 
-
-
-
-
 </div>
+</div>
+</div>
+  <div class="row pull-right">
+    <div class="col-lg-12 padreset">
+<?php
+  include "rightPanel.php";
+?>
+</div>
+</div> -->
+<!-- evvelki kodlar  -->
+
 
 <script type="text/javascript">
 // var like="a";
  function userLiked (terminID, terminLike, act) {
-  
-      
+
+
       console.log("Pressed like ", act);
-      
+
       $.ajax({
         url: 'like.php',
         type:'POST',
@@ -88,18 +131,18 @@
             console.log("entered function", data+"azik");
             console.log("azik");
             if (data == 'Success') {
-            
+
                 $('#num_like').html(++terminLike);
                 console.log(data);
             }
-            else 
+            else
               alert(data);
         }
       });
 }
 
 function handle(data) {
-      
+
 }
 </script>
 
