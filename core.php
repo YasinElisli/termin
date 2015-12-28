8484231a6b364b5d7eff294cee0cdd0c825eeca5
azik <azer.mehdiyev28@gmail.com>
2015-12-28 16:53:17 +0400

    Menbe elave olundu, amma random termin ashagi dushdu

--- core.php
+++ core.php
@@ -28,11 +28,17 @@ function addTermin(){
     <option value="Tibb">Tibb</option>
     <option value="Kimya">Kimya</option>
     <option value="Riyaziyyat">Riyaziyyat</option>
+    <option value="Mədəniyyət">Mədəniyyət</option>
+    <option value="Tarix">Tarix</option>
+    <option value="Coğrafiya">Coğrafiya</option>
+    <option value="Biologiya">Biologiya</option>
+    <option value="Fizika">Fizika</option>
+    <option value="Neft">Neft</option>
   </select>
 </div>
 <div class="form-group">
   <label for="source">Mənbə</label>
-  <input type="text" class="form-control" name="source" id="source" placeholder="Əgər varsa">
+  <input type="text" class="form-control" name="termin_source" id="source" placeholder="Əgər varsa">
 </div>
 <div class="form-group">
   <label for="keyWord">Açar söz (arasında vergül qoymaqla)</label>
@@ -46,10 +52,9 @@ function myTermin(){
   $start = 0;
   $limit = 3;
 
-  if(isset($_GET['id']))
-  {
-  $id=$_GET['id'];
-  $start=($id-1)*$limit;
+  if(isset($_GET['id'])) {
+    $id=$_GET['id'];
+    $start=($id-1)*$limit;
   }
 
   $userID = $_SESSION['user_id'];
@@ -231,6 +236,10 @@ function tags(){
   			$termin = $_POST['termin'];
   			$termin_desc = $_POST['termin_desc'];
   			$ter_cat = $_POST['ter_cat'];
+        $termin_source = $_POST['termin_source'];
+        //eger boshdursa onda userid adi menbe olacag
+        if (empty($termin_source)) $termin_source = $_SESSION['username'];
+        
         if (empty($termin) || empty($termin_desc)) {
           echo "Xanaları boş buraxmayın";
         }
@@ -247,8 +256,10 @@ function tags(){
             } else {
 
               $user=$_SESSION['user_id'];
+
               $today = date("Y-m-d");
-                $add = "INSERT INTO termin(user_id,termin, termin_desc, ter_cat,ter_pub_date) VALUES('$user','$termin', '$termin_desc', '$ter_cat','$today')";
+                $add = "INSERT INTO termin(user_id,termin, termin_desc, ter_cat,ter_pub_date, ter_source)
+                               VALUES('$user','$termin', '$termin_desc', '$ter_cat','$today', '$termin_source')";
                 $insert = mysqli_query($db_connection,$add);
 
                 if($insert){
