44f328c83d956217ea4152a783135348d41c9039
azik <azer.mehdiyev28@gmail.com>
2015-12-30 17:50:09 +0400

    user hover edende ve yeni melumati bazaya daxil edende stil deyishir

--- profile/my_termin.php
+++ profile/my_termin.php
@@ -1,4 +1,8 @@
+
+<link rel="stylesheet" type="text/css" href="css/profile.css">
 <?php
+
+
 if (isset($_SESSION['username'])) {
 	myTermin();
 }
@@ -9,16 +13,27 @@ else {
 
  <script type="text/javascript">
  $(document).ready(function() {
- 	$("a[contenteditable=true]").blur(changeDbValues);
+ 	//adding callbacks specifying what to do when the change is done
+ 	$("a[contenteditable=true]").blur(changeDbValues).hover(onMouseOver, onMouseOut);
+ 	$(".desct").blur(changeDbValues).hover(onMouseOver, onMouseOut);
 
- 	$(".desct").blur(changeDbValues);
+	// $("a[contenteditable=true]").hover(onMouseOver, onMouseOut);
  })
+
+ var onMouseOver = function() {
+ 	$(this).css('background-color','#ffffd3');
+ }
+
+var onMouseOut = function() {
+ 	$(this).css('background-color','#fff');
+ }
 
  var changeDbValues = function(){
  		// alert($(this).text());
  		var modifiedTerm = $(this).text();
-
  		var idAndColumn = $(this).attr("id");
+ 		// $(this).animate({left:'250px'}, "slow");
+ 		var element = $(this);
 		console.log(modifiedTerm, idAndColumn);
  		$.ajax({
  			type:"POST",
@@ -26,6 +41,13 @@ else {
  			data:'termin='+modifiedTerm+'&idAndColumn='+idAndColumn,
  			success: function(data) {
  				console.log(data);
+ 				console.log(element);
+
+ 				
+ 				// element.css('transition','background-color 3s');
+ 				// element.css('background-color','#5cb85c');
+ 				// element.css('background-color','#fff');
+ 				// $(this).animate({"left":"50px"}, 1000);
  				// if (data == "Success")
 
  			},
@@ -33,6 +55,15 @@ else {
  				console.log(jqXHR, errorThrown);
  			}
  		});
- 			
+ 		element.css('background-color','#5cb85c');
+
+ 		
+ 		setTimeout(
+ 			function(){
+ 				element.css('transition','background-color 2s');
+ 				element.css('background-color','#fff');
+ 			},500);
+
+ 		element.css('transition','');
  	}
  </script>
