dedaa53b22c225b5c15350db0f0dc357461096c6
Yasin Elisli <e.yasin@code.edu.az>
2016-01-04 18:07:05 +0400

    admin panel dizayni elave olundu

---
+++ admin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Panel</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+     <link rel="stylesheet" href="css/admin.css" media="screen" title="no title" charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row adminMain">
+        <div class="col l2 left-panel">
+          <div class="collection leftCollection">
+            <a href="#!" class="collection-item">Terminler<span class="badge">1</span></a>
+            <a href="#!" class="collection-item">Yeni terminlər<span class="new badge">4</span></a>
+            <a href="#!" class="collection-item">Menular</a>
+            <a href="#!" class="collection-item">Kateqoriya<span class="badge">14</span></a>
+          </div>
+        </div>
+        <div class="col l10 right-panel">
+          <div class="fixed-action-btn horizontal" style="bottom: 45px; right: 24px;">
+        <a class="btn-floating btn-large red">
+     <i class="large material-icons">edit</i>
+   </a>
+          <ul>
+            <!-- <li><a class="btn-floating red"><i class="material-icons">queue</i></a></li> -->
+            <li><a class="btn-floating yellow darken-1"><i class="material-icons">search</i></a> Termin axtar</li>
+            <li><a class="btn-floating green"><i class="material-icons">perm_identity</i></a> Userler</li>
+            <li><a class="btn-floating blue"><i class="material-icons">add</i></a> Termin Elave Et</li>
+          </ul>
+ </div>
+       </div>
+     </div>
+      </div>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+  </body>
+</html>
